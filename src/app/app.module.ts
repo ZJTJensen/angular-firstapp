fed7f4f1768c434b2164174b1a171bcf3b3baf99
Zachary Jensen <ZJTJensen@yahoo.com>
2017-12-05 12:00:27 -0500

    first commi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HelloworldComponent } from './helloworld/helloworld.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloworldComponent
   ],
   imports: [
     BrowserModule
